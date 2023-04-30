d319d02854c2da9b857c880f9304ffaee13a2446
Ainash <turbayeva17@gmail.com>
2023-04-30 22:05:59 +0600

    create game init logic, add input, output streams

--- Sudoku Game/main.swift
+++ Sudoku Game/main.swift
@@ -230,3 +230,115 @@ class Sudoku {
         print(buildMap())
     }
 }
+
+class Game {
+    static let main_menu = 0
+    static let game_levels = 1
+    static let instruction_page = 2
+    static let game = 3
+    
+    let menu = ["New Game", "Instructions", "Exit"]
+    let levels = ["Easy", "Medium", "Hard"]
+    let k_level_matcher = [30, 45, 55]
+    let sudoku_width = 9
+    
+    var step: Int
+    var started: Bool
+    var sudoku: Sudoku?
+    var validationError: String
+    
+    init() {
+        self.step = 0
+        self.started = false
+        self.validationError = ""
+    }
+    
+    func isStarted() -> Bool {
+        return self.started
+    }
+    
+    func setStarted(_ started: Bool) {
+        self.started = started
+    }
+    
+    func start(level: Int) {
+        self.sudoku = Sudoku(n: sudoku_width, k: k_level_matcher[level - 1])
+        self.sudoku?.fillValues()
+        self.started = true
+    }
+    
+    func printScreen() -> String {
+        var screen = ""
+        let back_menu_txt = "(enter 'MENU' to return to the main menu)"
+        switch (self.step) {
+        case Game.main_menu:
+            for i in 0..<menu.count {
+                let key = (i == menu.count - 1) ? "q" : String(i + 1)
+                screen += "\(i + 1) \(menu[i]) (PRESS \(key))\n"
+            }
+            break
+        case Game.game_levels:
+            screen = "Choose Level:\n"
+            for i in 0..<levels.count {
+                screen += "\(i + 1) \(levels[i]) (PRESS \(i + 1))\n"
+            }
+            screen += back_menu_txt
+            break
+        case Game.instruction_page:
+            screen = "About Game:\n"
+            screen += "A Sudoku puzzle begins with a grid in which some of the numbers are already in place.\n"
+            screen += "A puzzle is completed when each number from 1 to 9 appears only once in each of the rows, columns, and blocks.\n"
+            screen += "Study the grid to find the numbers that might fit into each cell.\n\n"
+            screen += "How to play:\n"
+            screen += "To fill in a number, enter the row number, column number, and value of the number separated by spaces. For example: '1 2 5'.\n"
+            screen += "This will place the number 5 in the second column of the first row.\n"
+            screen += "Make sure that the number you enter follows the rules of Sudoku.\n"
+            screen += "Each row, column, and 3x3 box must contain all of the numbers from 1 to 9 without any repeats.\n"
+            screen += "Enter 0 to give up and run Sudoku solver to see the answers.\n"
+            screen += back_menu_txt
+            break
+        case Game.game:
+            screen = self.sudoku!.buildMap() + "\n"
+            screen += "Enter 0 to give up and run Sudoku solver to see the answers" + "\n"
+            screen += "Enter 'MENU' to return to the main menu" + "\n"
+            screen += "Enter 'q' to quit" + "\n"
+            screen += "Enter row, column, and guessed number (e.g. '1 1 3'), or 'q' to quit: \n"
+            
+            if !self.validationError.isEmpty {
+                screen += self.validationError + "\n"
+            }
+            
+            self.validationError = ""
+            
+            break
+        default:
+            screen = "Welcome to the game\(back_menu_txt)"
+        }
+        
+        return screen
+    }
+    
+    func setValidationError(_ message: String) {
+        self.validationError = message
+    }
+    
+    func validateChoice(_ choice: Int) -> Bool {
+        if self.step == Game.main_menu && choice < menu.count {
+            return true
+        }
+        
+        if self.step == Game.game_levels && choice < levels.count {
+            return true
+        }
+        
+        return false
+    }
+    
+    func setStep(_ step: Int) {
+        self.step = step
+    }
+    
+    func getStep() -> Int {
+        return self.step
+    }
+}
